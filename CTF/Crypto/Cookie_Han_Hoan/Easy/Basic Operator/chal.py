87f2817f41b16b57e62f876a80353ef4af2f635f
Devotion1a1 <107429242+Devotion1a1@users.noreply.github.com>
2024-04-24 21:09:45 +0700

    Add files via upload

---
+++ CTF/Crypto/Cookie_Han_Hoan/Easy/Basic Operator/chal.py
@@ -0,0 +1,51 @@
+import math
+
+from Crypto.Util import number
+
+def padding_pkcs7(data,block_size=4):
+	tmp = len(data) + (block_size - len(data) % block_size)
+	print("pad")
+	print(data.ljust(tmp,bytes([block_size-(len(data)%block_size)])))
+	return data.ljust(tmp,bytes([block_size-(len(data)%block_size)]))
+
+def split_block(data,block_size):
+	print("split")
+	print(list(int.from_bytes(data[i:i+block_size],'little') for i in range(0,len(data),block_size)))
+	return list(int.from_bytes(data[i:i+block_size],'little') for i in range(0,len(data),block_size))
+
+def plus_func(data,shift):
+	print("plus")
+	print((data+shift)&0xffffffff)
+	return (data+shift)&0xffffffff
+
+def mul_func(data,mul):
+	print("mul")
+	print((data*mul)&0xffffffff)
+	return (data*mul)&0xffffffff
+
+def xor_shift_right_func(data,bit_loc):
+	print("xor")
+	print((data^(data>>bit_loc))&0xffffffff)
+	return (data^(data>>bit_loc))&0xffffffff
+
+def pow_func(data,e,p):
+	print("pow")
+	print(pow(data,e,p))
+	return pow(data,e,p)
+
+def exp_func(data,base,p):
+	print("exp")
+	print(pow(base,data,p))
+	return pow(base,data,p)
+
+def ecb_mode(data):
+	return list(pow_func(exp_func(xor_shift_right_func(mul_func(plus_func(block,3442055609),2898124289),1),e,p),e,p) for block in split_block(padding_pkcs7(data,4),4))
+
+if __name__=='__main__':
+	p = 1341161101353773850779
+	e = 2
+	mess = b'CHH{w3lc0m3_70_7h3_m47h_w0rld(1_h4t3_1t_th3r3)}'
+	cipher_flag = ecb_mode(mess)
+	print(cipher_flag)
+
+
---
+++ CTF/Crypto/Cookie_Han_Hoan/Easy/Basic Operator/solve.py
@@ -0,0 +1,53 @@
+from Crypto.Util.number import bytes_to_long, long_to_bytes
+from sympy.ntheory import discrete_log
+
+def rev_pow_func(result, p):
+    root1 = pow(result, (p + 1) // 4, p)
+    root2 = p - root1
+    return root1, root2
+
+
+def rev_exp_func(ouput, base, p):
+    return discrete_log(p, ouput, base)
+
+
+def rev_xor_func(y):
+    x = y
+    for i in range(1000):
+        if x ^ (x>>1) & 0xffffffff == y:
+            return x
+        x = x ^ (x>>1)
+
+    return -1
+
+def rev_mul_func(y):
+    x = y
+    for i in range(2**32):
+        if (x * 2898124289 & 0xffffffff) == y:
+            return (x)
+        x = (x * 2898124289) & 0xffffffff
+
+    return None
+
+def rev_plus_func(y):
+    return (y - 3442055609) & 0xffffffff
+
+
+def rev_split_little(y):
+    return (long_to_bytes(y).decode())[::-1]
+
+
+p = 1341161101353773850779
+e = 2
+cipher = [752589857254588976778, 854606763225554935934, 102518422244000685572, 779286449062901931327, 424602910997772742508, 1194307203769437983433, 501056821915021871618, 691835640758326884371, 778501969928317687301, 1260460302610253211574, 833211399330573153864, 223847974292916916557]
+answer = ""
+for ouput in cipher:
+    if rev_xor_func(rev_exp_func(rev_pow_func(ouput, p)[0], e, p)) != -1:
+        #answer += (rev_split_little(rev_plus_func(rev_mul_func(rev_xor_func(rev_exp_func(rev_pow_func(ouput, p)[0], e, p))))))
+        answer += (rev_split_little(rev_plus_func(rev_mul_func(rev_xor_func(rev_exp_func(rev_pow_func(ouput, p)[0], e, p))))))
+    else:
+        #answer += (rev_split_little(rev_plus_func(rev_mul_func(rev_xor_func(rev_exp_func(rev_pow_func(ouput, p)[1], e, p))))))
+        answer += (rev_split_little(rev_plus_func(rev_mul_func(rev_xor_func(rev_exp_func(rev_pow_func(ouput, p)[1], e, p))))))
+    print(answer)
+
+
